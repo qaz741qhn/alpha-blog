55152d2ed1fc6b8e33c6aca3b1749b49f94cc0e2
Lee Ching <qaz741qhn@gmail.com>
2021-07-24 12:46:18 +0900

    Add admin and admin user access

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,14 @@
 class ArticlesController < ApplicationController
     
     before_action :set_article, only: [:show, :edit, :update, :destroy]
+    before_action :require_user, except: [:show, :index]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
     
     def show
     end
     
     def index
-        @articles = Article.all
+        @articles = Article.paginate(page: params[:page], per_page: 3)
     end
     
     def new
@@ -18,7 +20,7 @@ class ArticlesController < ApplicationController
 
     def create
         @article = Article.new(article_params)
-        @article.user = User.new
+        @article.user = current_user
         if @article.save
         #需要白名單可submit的內容，rails有保護機制
             flash[:notice] = "Article was created successfully."
@@ -51,5 +53,9 @@ class ArticlesController < ApplicationController
     
     def article_params
         params.require(:article).permit(:title, :description)
+    end
+
+    def require_same_user
+        redirect_to @article if current_user != @article.user && !current_user.admin?
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,25 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+
+  def index
+    @users = User.paginate(page: params[:page], per_page: 3)
+  end
 
   def new
     @user = User.new
   end
 
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:notice] = "Welcome to the alpha blog #{@user.username}"
+      session[:user_id] = @user.id
+      flash.now[:notice] = "Welcome to the alpha blog #{@user.username}"
       redirect_to articles_path
     else
       render 'new'
@@ -20,23 +27,36 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account information was successfully updated"
-      redirect_to articles_path
+      redirect_to @user
     else
       render 'edit'
     end
+  end
+
+  def destroy
+    @user.destroy
+    session[:user_id] = nil if @user == current_user
+    #不然會只刪掉user卻沒刪掉登入中的id
+    redirect_to articles_path
   end
 
   private
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    redirect_to @user if current_user != @user && !current_user.admin?
   end
 
 end
